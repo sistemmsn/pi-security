fea7cc002356738055a323734f0fa08c5e90becb
oakerele <otakerele@gmail.com>
2018-01-24 20:24:43 -0500

    Anotherr

--- index.js
+++ index.js
@@ -1,48 +1,50 @@
 'use strict';
 
-// var Gpio = require('onoff').Gpio,
-//   pir = new Gpio(24, 'in');
+var Gpio = require('onoff').Gpio,
+  pir = new Gpio(24, 'in');
 
-// pir.read(function(err, value) {
-//   if (err) {
-//     throw err;
-//   }
+setInterval(function () {
+  pir.read(function(err, value) {
+    if (err) {
+      throw err;
+    }
 
-//   console.log(value);
+    console.log(value);
+  });
+}, 500);
+
+// var Gpio = require('pigpio').Gpio;
+// var trigger = new Gpio(23, {
+//   mode: Gpio.OUTPUT
+// });
+// var echo = new Gpio(24, {
+//   mode: Gpio.INPUT,
+//   alert: true
 // });
 
-var Gpio = require('pigpio').Gpio;
-var trigger = new Gpio(23, {
-  mode: Gpio.OUTPUT
-});
-var echo = new Gpio(24, {
-  mode: Gpio.INPUT,
-  alert: true
-});
+// // The number of microseconds it takes sound to travel 1cm at 20 degrees celcius
+// var MICROSECDONDS_PER_CM = 1e6 / 34321;
 
-// The number of microseconds it takes sound to travel 1cm at 20 degrees celcius
-var MICROSECDONDS_PER_CM = 1e6 / 34321;
+// trigger.digitalWrite(0); // Make sure trigger is low
 
-trigger.digitalWrite(0); // Make sure trigger is low
+// (function () {
+//   var startTick;
 
-(function () {
-  var startTick;
+//   console.log("Started program");
+//   echo.on('alert', function (level, tick) {
+//     var endTick, diff;
+//     console.log(level, tick);
+//     if (level == 1) {
+//       startTick = tick;
+//     } else {
+//       endTick = tick;
+//       diff = (endTick >> 0) - (startTick >> 0); // Unsigned 32 bit arithmetic
+//       console.log(diff / 2 / MICROSECDONDS_PER_CM);
+//     }
+//   });
+// }());
 
-  console.log("Started program");
-  echo.on('alert', function (level, tick) {
-    var endTick, diff;
-    console.log(level, tick);
-    if (level == 1) {
-      startTick = tick;
-    } else {
-      endTick = tick;
-      diff = (endTick >> 0) - (startTick >> 0); // Unsigned 32 bit arithmetic
-      console.log(diff / 2 / MICROSECDONDS_PER_CM);
-    }
-  });
-}());
-
-// Trigger a distance measurement once per second
-setInterval(function () {
-  trigger.trigger(10, 1); // Set trigger high for 10 microseconds
-}, 1000);
+// // Trigger a distance measurement once per second
+// setInterval(function () {
+//   trigger.trigger(10, 1); // Set trigger high for 10 microseconds
+// }, 1000);
