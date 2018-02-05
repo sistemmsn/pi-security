ebb3bd06b8ce91136a954da80894a17a9496bbfe
oakerele <otakerele@gmail.com>
2018-02-05 03:32:39 -0500

    Remove lapse

--- src/camera.js
+++ src/camera.js
@@ -2,16 +2,14 @@ var RaspiCam = require("raspicam");
 var Promise = require("promise");
 
 var camera = new RaspiCam({
-  // mode: "photo",
-	mode: "timelapse",
+  mode: "photo",
   output: __dirname + "/output/room-pi_%02d.jpg",
   quality: 100,
   width: 1920,
   height: 1080,
   rotation: 90,
 	encoding: "jpg",
-	timelapse: 3000, // take a picture every 3 seconds
-	timeout: 12000 // take a total of 4 pictures over 12 seconds
+	timeout: 0
 });
 
 exports.captureImage = () => {
