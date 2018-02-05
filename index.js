12e04fb2cec89ad2a7906a45327b2c43816e5acb
oakerele <otakerele@gmail.com>
2018-02-05 01:30:29 -0500

    Change to SR501

--- index.js
+++ index.js
@@ -1,41 +1,17 @@
 'use strict';
 
+// Code snippet from 
 var Gpio = require('pigpio').Gpio;
-var trigger = new Gpio(11, {  // 23
-  mode: Gpio.OUTPUT
-});
-var echo = new Gpio(8, {      // 24
+var sensor = new Gpio(23, {
   mode: Gpio.INPUT,
   alert: true
 });
 
-// The number of microseconds it takes sound to travel 1cm at 20 degrees celcius
-var MICROSECDONDS_PER_CM = 1e6 / 34321;
-
-trigger.digitalWrite(0); // Make sure trigger is low
-
-(function () {
+(function checkForObstacle() {
   var startTick, distance = 0;
 
-  console.log("Started program");
-  echo.on('alert', function (level, tick) {
-    var endTick, diff;
-    
-    if (level == 1) {
-      startTick = tick;
-    } else {
-      endTick = tick;
-      diff = (endTick >> 0) - (startTick >> 0); // Unsigned 32 bit arithmetic
-      distance = diff / 2 / MICROSECDONDS_PER_CM;
-    }
-
-    if (distance > 2 && distance < 300) {
-      console.log(distance);
-    }
+  sensor.on('alert', function (level, tick) {
+    console.log(level, tick);
+    console.log("Motion Detected");ß
   });
-}());
-
-// Trigger a distance measurement once per second
-setInterval(function () {
-  trigger.trigger(10, 1); // Set trigger high for 10 microseconds
-}, 100);
+})();
