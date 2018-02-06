79f134d9d9be0dbd8481f04fec589a1e1dc64db6
oakerele <otakerele@gmail.com>
2018-02-05 20:28:31 -0500

    Upload data and add to db

---
+++ src/data-uploader.js
@@ -0,0 +1,34 @@
+var fs = require('fs');
+var Promise = require("promise");
+var admin = require('firebase-admin');
+
+const serviceAccount = require("../config/firebase_acct.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: process.env.DATABASE_URL
+});
+const storage = admin.storage();
+const db = admin.database();
+
+exports.uploadImage = (filename, location) => {
+  const curTime = new Date().getTime() / 1000;
+  const newName =  curTime + ".jpg";
+  const bucket = storage.bucket("images");
+  const destination = `${location}/${newName}`;
+
+  return bucket.upload(filename, { destination: destination })
+    .then(res => {
+      var imageRef = db.ref(`images/${location}`);
+
+      bucket.file(destination).getMetadata().then(results => {
+        const metadata = results[0];
+        const imageUrl = metadata.selfLink;
+        imageRef.set({
+          imageUrl: imageUrl,
+          timestamp: curTime,
+          name: newName
+        })
+      })
+    });
+}
