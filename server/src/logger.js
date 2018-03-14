560df1eb1212faeff11ae1f4f46884e0e883cebf
oakerele <otakerele@gmail.com>
2018-03-14 18:26:31 -0400

    wrong method

--- server/src/logger.js
+++ server/src/logger.js
@@ -1,11 +1,11 @@
 const fs = require('fs');
 const path = require('path');
 const root = __dirname.split("src")[0];
-const logFilePath = path.join(root, '/images.log'); // Amateur hour shit
+const logFilePath = path.join(root, '/output .log'); // Amateur hour shit
 
 var log_file = fs.createWriteStream(logFilePath, {
   flags: 'a'
-});x
+});
 
 exports.logMotion = (data) => {
   const destDir = `${root}/log/`;
@@ -13,11 +13,14 @@ exports.logMotion = (data) => {
   const timestamp = data.timestamp;
 
   fs.access(destDir, (err) => {
-    if (err) fs.mkdirSync(destDir);
+    if (err) {
+      // If the directory does not exist, create a director
+      fs.mkdirSync(destDir);
+    }
 
-    copyFile(`${__dirname}/output/${data.filename}`, path.join(destDir, `${data.timestamp}.jpg`));
+    transferFile(`${__dirname}/output/${data.filename}`, path.join(destDir, `${data.timestamp}.jpg`));
   });
-  log_file.write(`${data.filename} was logged at ${new Date(data.timestamp).toLocaleDateString()}\n`)
+  log_file.write(`${data.filename} was logged at ${new Date(data.timestamp).toLocaleDateString()}\n`);
 }
 
 const transferFile = (src, dest) => {
